45bdb61a00f5c5de9ba565f2d772cf4e55d83e1c
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-29 00:14:43 +0200

    importerd
    importerd store

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const articleApi = createApi({
-    reducerPath: 'articleApi'
+    reducerPath: 'articleApi',
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => 'test'
+        })
+    })
 })
