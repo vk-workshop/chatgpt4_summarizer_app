aac0e1175c34f771f005173b6c0c64be39655c3e
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-28 20:20:33 +0200

    Update Hero.jsx
    added img, button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
-// import { logo } from "../assets"
+import { logo } from "../assets"
 
 const Hero = () => {
   return (
-    <header className="w-full flex justify-center items-center flex-col">Hero</header>
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="flex justify-between items-center flex-col">
+            <img src={logo} alt="sumz_logo" className="w-28 object-contain"/>
+            <button 
+            type="button"
+            onClick={() => window.open('https://github.com/VolodymyrKolisnichenko')}
+            className="black_btn">
+                GitHub
+            </button>
+        </nav>
+    </header>
   )
 }
 
