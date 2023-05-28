ac6fd87d4df16c204095956a5c32e60b15170489
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-28 21:31:33 +0200

    Update Demo.jsx
    created const [article, setArcticle] = useState, handleSubmit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,48 @@
-// import React from 'react'
+import { useState, useEffect } from "react"
+import { copy, linkIcon, loader, tick } from "../assets"
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
+    const [article, setArcticle] = useState({
+        url: '',
+        summary: '',
+    });
+    const handleSubmit = async (e) => {
+        alert('submit')
+    }
+    return (
+        <section className="mt-16 w-full max-w-xl">
+            <div className="flex flex-col w-full gap-2">
+                <form
+                    className="relative flex justify-center items-center"
+                    onSubmit={() => {handleSubmit}}>
+                    <img
+                        src={linkIcon}
+                        alt="link_icon"
+                        className="absolute left-0 my-2 ml-3 w-5">
+                    </img>
+                    <input
+                        type="url"
+                        placeholder="Enter a URL"
+                        value=""
+                        onChange={(e) => setArcticle({ ...article,
+                        url: e.target.value})}
+                        required
+                        className="url_input peer"
+                    />
+                    
+                    <button
+                        type="submit"
+                        className="submit_btn 
+                        peer-focus:border-gray-700 
+                        peer-focus:text-gray-700">
+                            ↵
+                    </button>
+                </form>
+                    {/* Browse URL history*/}
+            </div>
+            {/* Display Results */}
+        </section>
+    )
 }
 
 export default Demo
