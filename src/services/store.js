a1a786db7531503d7eb7183b32acada5eb9319db
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-28 23:47:39 +0200

    created
    created articleApi

--- src/services/store.js
+++ src/services/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { articleApi } from './article'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer
+    },
     middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat()
+    getDefaultMiddleware().concat(articleApi.middleware)
 })
