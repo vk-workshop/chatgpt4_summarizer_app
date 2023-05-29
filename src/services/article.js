f4cd345f9f775cada60e630039f514d180d9a6bd
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-29 09:57:02 +0200

    Update article.js
    copied from rapid api X-RapidAPI-Key and X-RapidAPI-Host

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+// const rapidApiKey =
+
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'KEY')
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com')
+        }
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => 'test'
