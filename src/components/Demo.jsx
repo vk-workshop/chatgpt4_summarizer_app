c4888dc5ae03a7581fba074c68fbf6d766035f17
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-30 14:54:23 +0200

    added
    added comments

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,7 +3,7 @@ import { copy, linkIcon, loader, tick } from "../assets"
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-    const [article, setArcticle] = useState({
+    const [article, setArticle] = useState({
         url: '',
         summary: '',
     });
@@ -11,10 +11,14 @@ const Demo = () => {
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
     const handleSubmit = async (e) => {
+        e.preventDefault();
+
         const { data } = await getSummary({ articleUrl: article.url })
         if (data?.summary) {
             const newArticle = { ...article, summary: data.summary };
-            setArcticle(newArticle)
+
+            setArticle(newArticle)
+            console.log(newArticle)
         }
     }
     return (
