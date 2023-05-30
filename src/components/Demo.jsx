ff41890a66b47e300ad1e451e0f744c86e1a4f11
VolodymyrKolisnichenko <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-30 14:38:16 +0200

    Update Demo.jsx
    created newArticle

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,20 +1,28 @@
 import { useState, useEffect } from "react"
 import { copy, linkIcon, loader, tick } from "../assets"
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
     const [article, setArcticle] = useState({
         url: '',
         summary: '',
     });
+
+    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
     const handleSubmit = async (e) => {
-        alert('submit')
+        const { data } = await getSummary({ articleUrl: article.url })
+        if (data?.summary) {
+            const newArticle = { ...article, summary: data.summary };
+            setArcticle(newArticle)
+        }
     }
     return (
         <section className="mt-16 w-full max-w-xl">
             <div className="flex flex-col w-full gap-2">
                 <form
                     className="relative flex justify-center items-center"
-                    onSubmit={() => {handleSubmit}}>
+                    onSubmit={() => { handleSubmit }}>
                     <img
                         src={linkIcon}
                         alt="link_icon"
@@ -24,21 +32,23 @@ const Demo = () => {
                         type="url"
                         placeholder="Enter a URL"
                         value={article.url}
-                        onChange={(e) => setArcticle({ ...article,
-                        url: e.target.value})}
+                        onChange={(e) => setArcticle({
+                            ...article,
+                            url: e.target.value
+                        })}
                         required
                         className="url_input peer"
                     />
-                    
+
                     <button
                         type="submit"
                         className="submit_btn 
                         peer-focus:border-gray-700 
                         peer-focus:text-gray-700">
-                            ↵
+                        ↵
                     </button>
                 </form>
-                    {/* Browse URL history*/}
+                {/* Browse URL history*/}
             </div>
             {/* Display Results */}
         </section>
